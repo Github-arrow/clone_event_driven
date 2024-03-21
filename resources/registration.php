7e12403b6c79791751ae5171197737d07e63de7d
Github-arrow <brian.d.pelarez@outlook.ph>
2024-03-21 13:27:14 +0800

    Version 1.2

--- resources/registration.php
+++ resources/registration.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-    <script src="./assets/js/search.js"></script>
+    <script src="../assets/js/search.js"></script>
 
-    <link href="./assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <script src="./assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <style>
         body {
@@ -22,7 +22,7 @@
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="javascript:void(0)">
-                <img src="./assets/img/logo.png" height="30">
+                <img src="../assets/img/logo.png" height="30">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
                 <span class="navbar-toggler-icon"></span>
@@ -30,7 +30,7 @@
             <div class="collapse navbar-collapse" id="mynavbar">
                 <ul class="navbar-nav me-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="./index.php">Dashboard</a>
+                        <a class="nav-link" href="../index.html">Dashboard</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="./registration.php">Registration</a>
@@ -45,7 +45,7 @@
         <p>You can add record for student here.</p>
         <div class="card mt-3">
 
-            <form action="./models/save.php" method="POST">
+            <form action="../models/save.php" method="POST">
                 <div class="card-header h3">Registration Form</div>
                 <div class="card-body">
                     <?php
